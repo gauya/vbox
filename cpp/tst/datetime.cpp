58be5458ad3d4b7d220a6e46f6f4ab6755e600e7
gau <hwang.chiyeong@google.com>
2025-10-08 09:08:31 +0900

    datetime

--- cpp/tst/datetime.cpp
+++ cpp/tst/datetime.cpp
@@ -17,14 +17,16 @@ int main() {
 
     // 현재 시스템 시간
     auto now = std::chrono::system_clock::now();
+    std::cout << std::format("현재 시간(UTC): {:%Y-%m-%d %H:%M:%S}\n", now);
     
     // time_t로 변환
-//    auto time = std::chrono::system_clock::to_time_t(now);
+    auto time = std::chrono::system_clock::to_time_t(now);
     
     // 로컬 시간으로 출력
-//    std::cout << std::format("현재 시간: {:%Y-%m-%d %H:%M:%S}\n", *std::localtime(&time));
+//    std::cout << std::format("현재 시간: {:%Y-%m-%d %H:%M:%S}\n", std::localtime(&time));
+    
+    std::cout << std::put_time(std::localtime(&time),"현재 시간(KST): %Y-%m-%d %H:%M:%S\n");
 
-    std::cout << std::format("현재 시간: {:%Y-%m-%d %H:%M:%S}\n", now);
 
     return 0;
 }
